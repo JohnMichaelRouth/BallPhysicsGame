4f9513691ca7b47c7426d8444e4eba7323a5816a
Michael Routh <43078477+JohnMichaelRouth@users.noreply.github.com>
2023-09-30 10:27:25 -0500

    Added Title Screen

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnNewGamePressed()
+    {
+        SceneManager.LoadScene("Gameplay");
+        SceneManager.UnloadSceneAsync("TitleScreen");
+    }
+
+    public void OnExitPressed()
+    {
+        Debug.Log("Close Game");
+
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+}
